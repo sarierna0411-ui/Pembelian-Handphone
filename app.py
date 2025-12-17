8683e9ac80e700f3b3f910bc97320fd5893a287f
sarierna0411-ui <sarierna0411@gmail.com>
2025-12-17 14:19:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 import pandas as pd
 
-# Judul aplikasi
 print("=== AI Agent Pembelian Handphone ===")
 
-# Membaca data pembelian HP
+# Baca data
 data = pd.read_csv("data_hp.csv")
 
-# Menampilkan data
-print("\nData Pembelian Handphone:")
-print(data)
+# Input dari pengguna
+budget = int(input("Masukkan budget (Rp): "))
+
+# Logika AI Agent
+rekomendasi = data[data["harga"] <= budget]
+
+# Hasil
+print("\nRekomendasi Handphone:")
+if rekomendasi.empty:
+    print("Tidak ada handphone sesuai budget.")
+else:
+    print(rekomendasi)
